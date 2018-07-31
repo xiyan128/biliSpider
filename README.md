fe12e0f97a9c7b8483f8e20f0e9e929ce5361d58
ShaoXiyan <shaoxy128@foxmail.com>
2018-07-31 19:25:09 +0800

    add ReadMe file

---
+++ README.md
@@ -0,0 +1,5 @@
+# 哔哩哔哩爬虫
+
+spider.py: 只能爬一个区的数据，爬的页数得自己修改。小心被ban
+visualize.py: plotly可视化
+sort: 生产各种榜单
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-data = [go.Scatter(
-#           x=df['_id'],
-#           y=df['count'])]
-
-# py.iplot(data)
